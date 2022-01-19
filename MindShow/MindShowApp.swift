edc6958f2199f4f5ecc0e1a0758b993ff9c80b66
sunyanguo <sunyanguo@lvmama.com>
2022-01-19 14:02:18 +0800

    Initial Commit

---
+++ MindShow/MindShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindShowApp.swift
+//  MindShow
+//
+//  Created by yanguo sun on 2022/1/19.
+//
+
+import SwiftUI
+
+@main
+struct MindShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
